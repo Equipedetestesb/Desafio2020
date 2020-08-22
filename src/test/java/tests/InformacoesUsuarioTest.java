9b4b755fa01b4f00e07f88886a17321cdc77a59e
gem install bundler <jhonatagusmao3e@gmail.com>
2020-08-21 23:14:51 -0300

    Excluindo phone e tirando print.

--- src/test/java/tests/InformacoesUsuarioTest.java
+++ src/test/java/tests/InformacoesUsuarioTest.java
@@ -4,17 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import suporte.Generator;
+import suporte.Screenshot;
 
 import java.util.concurrent.TimeUnit;
 
 public class InformacoesUsuarioTest {
     //Declarando navegador para ser usado em todos os métodos desta classe
     private WebDriver navegador;
+
+    //Utilizado para invocar o nome da classe do @Test
+    @Rule
+    public TestName test = new TestName();
 
 
     //Notação para reduzir repetições dentro do código, assim antes de qualquer  @Test ele faz o que tem dentro do
@@ -30,11 +41,6 @@ public class InformacoesUsuarioTest {
 
         //Navegando para a páginado Taskit!
         navegador.get("http://www.juliodelima.com.br/taskit");
-    }
-
-    @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
-
 
         //Clicar no link que possui o texto Sign in
         WebElement linkSignIn = navegador.findElement(By.linkText("Sign in"));
@@ -53,45 +59,64 @@ public class InformacoesUsuarioTest {
 
         //Clicar no link com o texto "SIGN IN"
         navegador.findElement(By.linkText("SIGN IN")).click();
-        //Validar que dentro do elemento com class "me" está o texto "Hi Julio"
-        WebElement me = navegador.findElement(By.className("me"));
-        String textoNoElementoMe = me.getText();
-        assertEquals("Hi, Julio",textoNoElementoMe);
 
-        //Validação
+        //Clicar no link que possue a class "me"
+        navegador.findElement(By.className("me")).click();
 
+        //Clicar em um link que possui o texto "MORE DATA ABOUT YOU"
+        navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+
+    }
+
+    // @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
+
+        //Clicar no botão através do seu xpath //button[@data-target="addmoredata"]
+        navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
+
+        //Identificar a popup onde está o formulário de id
+        WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
+
+        //Na combo de name "type" escolher a opção "Phone"
+        WebElement campoType = popupAddMoreData.findElement(By.name("type"));
+        new Select(campoType).selectByVisibleText("Phone");
+
+        //No campo de name "contact" digitar "+5571991234567"
+        popupAddMoreData.findElement(By.name("contact")).sendKeys("+5571991234567");
+
+        //Clicar no link de text "SAVE" que está na popup
+        popupAddMoreData.findElement(By.linkText("SAVE")).click();
+
+        //Na mensagem de id "toast-container" validar que o texto é "Your contact has been added!"
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals("Your contact has been added!", mensagem);
 
     }
 
     @Test
-    public void testAdicionarUmaInformacaoAdicionalDoUsuario0002(){
+    public void removerUmContatoDeUmUsuario(){
 
+        //Clicar no elemento pelo seu xpath //span[text()="71988099999"]/following-sibling::a
+        navegador.findElement(By.xpath("//span[text()=\"+5561992141556\"]/following-sibling::a")).click();
 
-        //Clicar no link que possui o texto Sign in
-        WebElement linkSignIn = navegador.findElement(By.linkText("Sign in"));
-        linkSignIn.click();
+        //Confirmar a janela javascript
+        navegador.switchTo().alert().accept();
 
-        //Clicar no campo com o name "login" que está dentro do formulário de id "signinbox"
-        WebElement formularioSignInbox = navegador.findElement(By.id("signinbox"));
+        //Validar que a mensagem apresentada foi Rest in peace, dear phone!
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals("Rest in peace, dear phone!", mensagem);
 
-        //Digitar no campo com o name "login" que está dentro do formulário de id "signinbox" o texto "julio0001"
-        formularioSignInbox.findElement(By.name("login")).sendKeys("julio0002");
+        String screenshotArquivo = "/C:/Desafio2020/evidencias/" + Generator.dataHoraParaArquivo() + test.getMethodName() + ".png";
+        Screenshot.tirar(navegador, screenshotArquivo);
 
-        //Clicar no campo com o name "password" que está dentro do formulário de id "signinbox"
-        formularioSignInbox.findElement(By.name("password")).sendKeys("654321");
+        //Aguardar até 10 segundos para que a janela desapareça
+        WebDriverWait aguardar = new WebDriverWait(navegador, 10);
+        aguardar.until(ExpectedConditions.stalenessOf(mensagemPop));
 
-        //Clicar no campo com o name "password" que está dentro do formulário de id "signinbox" o texto "123456"
-
-        //Clicar no link com o texto "SIGN IN"
-        navegador.findElement(By.linkText("SIGN IN")).click();
-        //Validar que dentro do elemento com class "me" está o texto "Hi Julio"
-        WebElement me = navegador.findElement(By.className("me"));
-        String textoNoElementoMe = me.getText();
-        assertEquals("Hi, Julio",textoNoElementoMe);
-
-        //Validação
-
-
+        //Clicar no link com texto "Logout"
+        navegador.findElement(By.linkText("Logout")).click();
     }
 
     //Notação para reduzir repetições dentro do código, assim depois de qualquer @Test ele faz o que tem dentro do
@@ -99,6 +124,6 @@ public class InformacoesUsuarioTest {
     @After
     public void tearDown(){
         //Fechar o navegador
-        navegador.close();
+        // navegador.close();
     }
 }
